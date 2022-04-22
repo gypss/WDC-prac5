1b67220412e4cdaf1361a2a2eedb2d6e95fee002
G-SlBk <83564557+G-SlBk@users.noreply.github.com>
2022-04-22 13:31:37 +0000

    q35

--- query3-5.sql
+++ query3-5.sql
@@ -1,4 +1,4 @@
 SELECT given_name, family_name,mark FROM Students INNER JOIN Enrolments
 ON Students.student_id=Enrolments.student_id INNER JOIN Subjects
 ON Enrolments.subject_code=Subjects.subject_code
-WHERE mark<50;
+WHERE mark < 50 AND subject='Computer Systems';
