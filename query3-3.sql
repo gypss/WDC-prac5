da493ed22aeffcdab4ea5ea603f607ae1ac13ba2
G-SlBk <83564557+G-SlBk@users.noreply.github.com>
2022-04-22 12:35:04 +0000

    fix

--- query3-3.sql
+++ query3-3.sql
@@ -1,3 +1,1 @@
-SELECT given_name, family_name FROM Students INNER JOIN Enrolments ON Students.student_id=Enrolments.student_id
-    -> INNER JOIN Subjects ON Enrolments.subject_code=Subjects.subject_code
-    -> WHERE subject='Web and Database Computing';
+SELECT given_name, family_name FROM Students WHERE program = 'BCompSc';
